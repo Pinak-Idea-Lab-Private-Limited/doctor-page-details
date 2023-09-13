8d96a93168aaa2fa0081e17b99804bb069683f76
shaikat17 <shaikatpal56@gmail.com>
2023-09-13 10:53:11 +0600

    Alpine js read more functionality deleted. And rating modal not working problem solved.

--- index.js
+++ index.js
@@ -29,15 +29,18 @@ function renderStarRating(rating, ratingCard) {
 
     if (rating >= i) {
       // star with fill color
+      // star.classList.add("fa-star", "star-box");
       star.classList.add("fa-star", "star-box", "w-6", "h-6", "flex", "items-center", "content-center");
       star.style.background = `${color}`;
     } else if (i === Math.ceil(rating)) {
       // half fill color star
+      // star.classList.add("fa-star", "star-box");
       star.classList.add("fa-star", "star-box", "w-6", "h-6", "flex", "items-center", "content-center");
       star.style.backgroundImage = `linear-gradient(to right, ${color} 0% ${colorPercentage}%, #78716C ${grayPercentage}% 100%)`;
     } else {
       // star without fill color
       star.classList.add("fa-star", "star-box", "w-6", "h-6", "flex", "items-center", "content-center");
+      // star.classList.add("fa-star", "star-box");
       star.style.background = `#78716C`;
     }
 
@@ -48,12 +51,10 @@ function renderStarRating(rating, ratingCard) {
 const starsTotal = 5;
 document.addEventListener("DOMContentLoaded", getRatings);
 function getRatings() {
-  for (let rating in ratings) {
-    document.querySelector(
-      `.${rating} .number-rating`
-    ).innerHTML = `${ratings[rating]}`;
-    renderStarRating(ratings[rating], rating);
-  }
+  document.querySelector(
+    `.doc1 .number-rating`
+  ).innerHTML = `3.1`;
+  renderStarRating(3.1, 'doc1');
 }
 
 // // ABOUT content - add three dots when overflowing
@@ -71,38 +72,28 @@ function getRatings() {
 //   }
 // });
 
-  let readMoreBtn = document.querySelector('#aboutTab-rmBtn-0')
-  let content = document.querySelector('.content')
-  let readMoreParent = document.querySelector('#readMore-parent')
+let readMoreBtn = document.querySelector("#aboutTab-rmBtn-0");
+let content = document.querySelector(".content");
+let readMoreParent = document.querySelector("#readMore-parent");
 
-  readMoreBtn.addEventListener('click', () => {
-    if(readMoreBtn.innerText === 'Read More') {
-      content.classList.remove('h-[120px]')
-      content.style.height = 'min-content'
-      content.style.paddingBottom = "40px";
-      readMoreBtn.innerText = 'Read Less'
-      readMoreParent.classList.remove('right-0', 'w-[100%]', 'sm:w-[55%]')
-      readMoreParent.classList.add('left-0')
-    } else {
-      content.style.height = '120px'
-      content.classList.add('h-[120px]')
-      content.style.paddingBottom = "0px";
-      readMoreBtn.innerText = 'Read More'
-      readMoreParent.classList.add('right-0', 'w-[100%]', 'sm:w-[55%]')
-      readMoreParent.classList.remove('left-0')
-    }
-  })
+readMoreBtn.addEventListener("click", () => {
+  if (readMoreBtn.innerText === "Read More") {
+    content.classList.remove("h-[120px]");
+    content.style.height = "min-content";
+    content.style.paddingBottom = "40px";
+    readMoreBtn.innerText = "Read Less";
+    readMoreParent.classList.remove("right-0", "w-[100%]", "sm:w-[55%]");
+    readMoreParent.classList.add("left-0");
+  } else {
+    content.style.height = "120px";
+    content.classList.add("h-[120px]");
+    content.style.paddingBottom = "0px";
+    readMoreBtn.innerText = "Read More";
+    readMoreParent.classList.add("right-0", "w-[100%]", "sm:w-[55%]");
+    readMoreParent.classList.remove("left-0");
+  }
+});
 
-  const readbtn = document.querySelector('#readbtn')
-  readbtn.addEventListener('click', () => {
-    if(readbtn.innerText === 'Read More') {
-      readbtn.innerText = 'Read Less'
-      content.style.height = '900px'
-    } else {
-      readbtn.innerText = 'Read More'
-      readbtn.style.height = '200px'
-    }
-  })
 
 const showAndHideModal = (state) => {
   let dcModal = document.querySelector("#dc-modal");
@@ -110,12 +101,11 @@ const showAndHideModal = (state) => {
   let modalContent = document.querySelector("#modalContent");
 
   if (state === "show") {
-    modalTitle.innerText = document.querySelector('#about-header').innerText
+    modalTitle.innerText = document.querySelector("#about-header").innerText;
 
-    modalContent.innerHTML = document.querySelector('.about-content').innerHTML
+    modalContent.innerHTML = document.querySelector(".about-content").innerHTML;
 
     dcModal.classList.remove("hidden");
-
   } else {
     dcModal.classList.add("hidden");
   }
@@ -137,17 +127,23 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-
-
 // rating modal show and hide
 const ratingModalToggle = (state) => {
   let rModal = document.querySelector("#ratingInfo-modal");
 
-
   if (state === "show") {
     rModal.classList.remove("hidden");
-
   } else {
     rModal.classList.add("hidden");
   }
 };
+
+// const testFn = () => {
+//   console.log('Hello')
+// }
+
+// const ratingI = document.querySelector('#ratingI')
+
+// ratingI.addEventListener('click', () => {
+//   console.log("first")
+// })
